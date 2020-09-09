1822d30a7d8872e7d954831283ca2db958eab6c3
Raymundo Salazar <raymundo.salser@hotmail.com>
2020-09-09 12:22:57 -0500

    Update readme with troubleshooting and database guidelines and database config file fix

---
+++ app/Models/TestModel.php
@@ -0,0 +1,10 @@
+<?php
+class TestModel extends Model {
+    /**
+      * The database table used by the model.
+      *
+      * @var string
+      */
+    //here i should call the external database table
+    protected $connection = 'db_egobierno';
+}
