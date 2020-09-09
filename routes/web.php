4c7e98238775d5b93878ca17888d417fc638f595
jaimexcastillo <jaime.castilloc@nuevoleon.gob.mx>
2020-09-08 19:35:44 -0500

    removi la ruta de /listado

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::middleware(["validate_session"])->group(function(){
 		return redirect("/dashboard");
 	});
 	Route::get('/dashboard', "DashboardController@index");
-	Route::get('/listado', "ListadoController@index");
 	Route::get('/tramites/{type}', "TramitesController@index");
 	Route::get('/nuevo-tramite', "TramitesController@new");
 	
