ef4d46888c3bd4bfa0c472b1a2370212747e91a6
Adriana1705 <adriana.contreras@futurite.com>
2020-09-11 09:48:35 -0500

    fechas listado

--- app/Http/Controllers/TramitesController.php
+++ app/Http/Controllers/TramitesController.php
@@ -55,13 +55,13 @@ class TramitesController extends Controller
     public function listaTramites(){
 
       $tramits = $this->relationship->get();
-
+      //dd($tramits);
       $tmts = array();
 
       foreach ($tramits as $t) {
         $id_tramite = $t->tramite_id;
-        // $created_at = $t->created_at;
-        // $updated_at = $t->updated_at;
+        $created_at = $t->created_at;
+        $updated_at = $t->updated_at;
 
         $serv = $this->tiposer->where('Tipo_Code', $id_tramite)->get();
         foreach ($serv as $s) {
@@ -69,7 +69,9 @@ class TramitesController extends Controller
 
           $tmts []=array(
             'id_tramite' => $id_tramite,
-            'nombre' => $name
+            'nombre' => $name,
+            'created_at' => $created_at,
+            'updated_at' => $updated_at
           );
 
         }
