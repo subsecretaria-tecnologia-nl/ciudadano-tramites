e782cf696c7801518d9bc296901a973afaef3cbe
Raymundo Salazar <raymundo.salser@hotmail.com>
2020-09-15 12:30:42 -0500

    Error pages 404 and 500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Página no Encontrada | {{ Config::get("layout.args.title") }}</title>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,600;0,900;1,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="../css/app.css">
+</head>
+<body>
+	<div id="error" class="error-500">
+		<div class="error-text">
+			<h1>¡Algo salió mal!</h1>
+			<p>Hubo un error en servidor. Por favor intenta más tarde.</p>
+		</div>
+	</div>
+</body>
+</html>
