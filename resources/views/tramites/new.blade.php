b45390e9f3c20edf0ec15118b015bc54b5285ae7
Raymundo Salazar <raymundo.salser@hotmail.com>
2020-09-17 16:49:00 -0500

    remove search bar and top pading in new tramite

--- resources/views/tramites/new.blade.php
+++ resources/views/tramites/new.blade.php
@@ -1,4 +1,4 @@
-<div class="content d-flex flex-column flex-column-fluid contenido " id="kt_content">
+<div class="content d-flex flex-column flex-column-fluid contenido pt-0" id="kt_content">
     <div class="d-flex flex-column-fluid">
         <div class="container">
 
