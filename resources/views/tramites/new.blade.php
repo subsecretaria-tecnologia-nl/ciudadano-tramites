97e202d536ee22fe2f0e12b8489eba0c5d7f83d1
Alberto <alberto.martinezve@famsa.com>
2020-09-23 13:20:39 -0500

    nuevo diseño

--- resources/views/tramites/new.blade.php
+++ resources/views/tramites/new.blade.php
@@ -1,149 +1,94 @@
-
-<div id="buscador" class="">
-    <div class="busqueda">
-            
-    </div>
-    <div class="gradient">
-            <div class="form-group">
-                <div class="padding-busqueda">
-                    <span class="busqueda-titulo">Busca tus trámites</span>
-                </div>
-                <span  class="busqueda-sub">Puedes buscar por nombre de trámite, número de folio, nombre de solicitante, etc.</span>
-                <div class="padding-busqueda">
-                    <input class="form-control  form-control-lg" type="text" name="busqueda" id="busqueda"  placeholder="Ejemplo: Certificado de gravamen">
-                </div>
-            </div>
-    </div>
-</div>
-
-<div class="content d-flex flex-column flex-column-fluid contenido " id="kt_content">
+<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
     <div class="d-flex flex-column-fluid">
-        <div class="container">
-
-            <div class="tramites">
-                <div class="cuadros-cont">
-                    <div>
-                        <span > inicio->Tramites en curso->Selección de Trámite</span>
-                    </div>
-                    <div class="cuadro-listado tramite-lista" style="padding-top: 10px">
-                        <div class="titulo-cuadro">
-                            <div style="padding-top: 20px;">
-                                <span class="tituloTabla">
-                                    Selección de Trámite
-                                </span>
+        <div class="container">                
+            <div>
+                <span > inicio->Tramites en curso->Selección de Trámite</span>
+            </div>
+            <div class="card" style="padding-top: 10px">
+                <div class="card-body">
+                    <div style="padding-top: 20px;">
+                        <span class="card-title">
+                            
+                            <div style="padding-left: 1%">
+                                <span class="titulo">Selección de Trámite</span>
                             </div>
-                            <div class="dropdown-divider"></div>
-               
-							<section id="tabs" class="project-tab">
-							    <div class="container">
-							        <div class="row">
-							            <div class="col-md-12">
-							                <nav>
-							                    <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-							                        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">
-								                       	1._Selección de Trámite
-								                    </a>
-							                        <a class="nav-item nav-link" id="nav-detalle-tab" data-toggle="tab" href="#nav-detalle" role="tab" aria-controls="nav-detalle" aria-selected="false" onclick="buildTablaDetalles()">
-							                        	2._Detalle
-							                        </a>
-							                        <a class="nav-item nav-link" id="nav-comprobante-tab" data-toggle="tab" href="#nav-comprobante" role="tab" aria-controls="nav-comprobante" aria-selected="false">
-							                        	3.Comprobante
-							                        </a>
-							                    </div>
-							                </nav>
-							                <div class="tab-content" id="nav-tabContent">
-							                    <div class="tab-pane fade show active mx-auto" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" style="width: 70%;">
-
-													<form class="p-5" action="#!" id="formularioDinamico">
-													    <!-- Tramite -->
-													    <div class="row">
-														    <div class="col-lg-12" id="divSelectTramites" >
-														    	<div class="form-group">
-														            <label class="col-md-3 control-label">Trámite</label>
-														            <div>
-														                <select class="select2me form-control browser-default custom-select mb-4" name="tramitesSelect" id="tramitesSelect">
-														                	<option value="limpia">------</option>
-																    	</select>   
-														            </div>
-														        </div>
-														    </div>
-														</div>
-														<div class="text-center" id="loadin" style="display: none; margin-bottom: 9px;" >
-															<div class="spinner-grow" role="status">
-															  	<span class="sr-only">Loading...</span>
-															</div>
-														</div>
-													    <div id="camposDinamicosDiv" class="row"></div>
-
-													    <div class="text-center  ">
-														    <button class="btn btn-info"  type="button" onclick="next('#nav-detalle-tab')">
-							                        			Siguiente
-							                        		</button>
-													    </div>
-
-													</form>
-							                    </div>
-							                    <div class="tab-pane fade" id="nav-detalle" role="tabpanel" aria-labelledby="nav-detalle-tab">
-
-
-													<div class="offset-xl-2 col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 10px;">
-													     <div class="card">
-													         <div class="card-body">
-													             <div class="table-responsive-sm">
-													                 <table class="table table-striped" id="tableDetails">
-													                     <thead>
-													                         <tr>
-													                         	<th></th>
-													                            <th class="center">#</th>
-													                            <th>Nombre</th>
-													                            <th>Descripción</th>
-													                            <th class="right">Total</th>
-
-													                         </tr>
-													                     </thead>
-													                 </table>
-													             </div>
-													             <div class="row">
-													                 <div class="col-lg-4 col-sm-5">
-													                 </div>
-													                 <div class="col-lg-4 col-sm-5 ml-auto">
-													                     <table class="table table-clear">
-													                         <tbody>
-													                             <tr>
-													                                 <td class="left">
-													                                     <strong class="text-dark">Total</strong> </td>
-													                                 <td class="right">
-													                                     <strong class="text-dark">$20,744,00</strong>
-													                                 </td>
-													                             </tr>
-													                         </tbody>
-													                     </table>
-													                 </div>
-													             </div>
-													         </div>
-													     </div>
-													 </div>
-
-												    <div class="text-center  ">
-													    <button class="btn btn-info"  type="button" onclick="next('#nav-comprobante-tab')">
-						                        			Siguiente
-						                        		</button>
-												    </div>
-							                    </div>
-							                    <div class="tab-pane fade" id="nav-comprobante" role="tabpanel" aria-labelledby="nav-comprobante-tab">
-							                    		Comprbant
-							                    </div>
-							                </div>
-							            </div>
-							        </div>
-							    </div>
-							</section>
-
-
-                        </div>
+                        </span>
                     </div>
+                    <div class="dropdown-divider"></div>
+                    <section id="tabs" >
+					    <div class="container-fluid">
+
+
+							<div class="row">
+
+							    <!--Grid column-->
+							    <div class="col-lg-8">
+
+							      	<!-- Card -->
+							      	<div class="mb-3">
+							        	<div class="pt-4 wish-list" id="listTramites">
+							        	</div>
+							      	</div>
+							      	<!-- Card -->
+							    </div>
+							    <!--Grid column-->
+
+							    <!--Grid column-->
+							    <div class="col-lg-4">
+
+							    	<!-- Card -->
+							      	<div class="mb-3 shadow-sm p-3 bg-white rounded">  
+							        	<div class="pt-4 ">
+
+							          		<h5 class="mb-3">Total:</h5>
+
+							          		<ul class="list-group list-group-flush">
+							            		<li class="list-group-item d-flex justify-content-between align-items-center px-0 pb-0">
+							              			Subtotal
+							              			<span>$25.98</span>
+							            		</li>
+							            		<li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+							              			<div>
+							                			<strong>Total</strong>
+							                			<strong>
+							                  				<p class="mb-0">(incluy IVA)</p>
+							                			</strong>
+							              			</div>
+							              			<span><strong>$53.98</strong></span>
+							            		</li>
+							          		</ul>
+
+							          		<button id="metodoPagoBtn" type="button" class="btn btn-primary btn-block" onclick="metodoPago()">
+							          			Elegir método de pago
+							          		</button>
+							          		<button  id="addTramiteBTN" type="button" class="btn btn-default btn-block" onclick="openModalAdd()">Agregar Trámite</button>
+
+							        	</div>
+							     	</div>
+							      	<!-- Card -->
+
+
+							      	<!-- Card -->
+							      	<div class="mb-3 shadow-sm p-3 bg-white rounded" id="divMetodoPago"  style="display: none;">
+							        	<div class="pt-4" id="containerMetodoPago">     	</div>
+										<button type="button" class="btn btn-primary btn-block" onclick="pagar()"> Pagar </button>
+							        	<button id="metodoPagoBtn" type="button" class="btn btn-danger btn-block" onclick="cancelarPago()">
+							          		Cancelar
+							          	</button>
+							          	
+							      	</div>
+							      	<!-- Card -->
+							    </div>
+							    <!--Grid column-->
+
+							</div>
+							<!-- Grid row -->
+					    </div>
+					</section>
+
                 </div>
             </div>
+                
         </div>
         
     </div>
@@ -174,6 +119,49 @@
 	</div>
 </div>
 
+<div id="addTramite" class="modal fade " tabindex="-1" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true" ></button>
+                <h4 class="modal-title">Agregar tramité</h4>
+            </div>
+            <div class="modal-body">
+
+				<form class="p-5" action="#!" id="formularioDinamico">
+				    <!-- Tramite -->
+				    <div class="row">
+					    <div class="col-lg-12" id="divSelectTramites" >
+					    	<div class="form-group">
+					            <label class="col-md-3 control-label">Trámite</label>
+					            <div>
+					                <select class="select2me form-control browser-default custom-select mb-4" name="tramitesSelect" id="tramitesSelect">
+					                	<option value="limpia">------</option>
+							    	</select>   
+					            </div>
+					        </div>
+					    </div>
+					</div>
+					<div class="text-center" id="loadin" style="display: none; margin-bottom: 9px;" >
+						<div class="spinner-grow" role="status">
+						  	<span class="sr-only">Loading...</span>
+						</div>
+					</div>
+				    <div id="camposDinamicosDiv" class="row"></div>
+				</form>
+
+            </div>
+            <div class="modal-footer">
+	         	<button type="button" data-dismiss="modal" class="btn default" >Cancelar</button>
+	            <button type="button"  class="btn green" onclick="eliminar()" id="btnAdd">
+	            	<i class="fa fa-check" id="iconBtnAdd"></i> 
+	            	Aceptar
+	            </button>
+	        </div>
+	   </div>
+	</div>
+</div>
+
 <link href="{{ asset('css/newTramite.css') }}" rel="stylesheet" type="text/css" />
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -184,6 +172,11 @@
 <script type="text/javascript" src="{{ asset('js/nuevoTramite/ElementFactory.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/nuevoTramite/FormularioBuilder.js') }}"></script>
 
+<script type="text/javascript" src="{{ asset('js/nuevoTramite/JSONGeneraReferencia.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/nuevoTramite/JSONGeneraReferenciaBuilder.js') }}"></script>
+
+
+<script type="text/javascript" src="{{ asset('js/nuevoTramite/shoppingCarModule/shoppingCarBuilder.js') }}"></script>
 <script type="text/javascript">
 	let tramites = [];
 
@@ -191,13 +184,27 @@
 
 	$(document).ready(() => {
     	getTramites();
+
+		JSONGeneraReferenciaBuilder.setToken("9E1345508F2EBF30BD4D227D7E625111CFA6")
+									.setUrlRetorno("http://10.153.144.94/egobDes/CNAPRetorno.php")
+									.setImporteTransaccion(500)
+									.setIdTransaccion("201910")
+									.setEntidad("3")
+									.setUrlConfirmaPago("http://10.153.144.94/egobDes/CNAConfirma.php");
+
+    	
     });
+
+    function openModalAdd(){
+    	$("#tramitesSelect").val("limpia").trigger('change');
+    	$("#camposDinamicosDiv").empty();
+    	$("#addTramite").modal({show: true}); 
+    }
 
 	function getTramites(){
 		let url = "{{ url('/allTramites') }}";
 		getApi( url,false , ((response) => {  
         	tramites = JSON.parse(response);
-        	console.log(tramites )
         	setTramites( $("#tramitesSelect") );
         }), (( msg ) => {
          	Command: toastr.warning("No Success", "Notifications") ;
@@ -229,10 +236,11 @@
 	});
 
 	function getCampos(){
+		$("#btnAdd").attr("disabled", true);
 		let url = "{{ url('/getCampos') }}";
 		let data =  { id_tramite:$("#tramitesSelect").val()  } 
 		getApi( url, data , ((response) => {  
-        	buildForm( response, data.id_tramite );
+        	buildForm( response );
         }), (( msg ) => {
          	Command: toastr.warning("No Success", "Notifications") ;
         }) ,  () => { 
@@ -241,10 +249,15 @@
         });
 	}
 
-	function buildForm( campos, id_tramite ){
-		let arrToDOm = FormularioBuilder.build( campos, validarForm, id_tramite );
-		$("#camposDinamicosDiv").hide().empty().append( arrToDOm ).fadeIn(100);                                               
+	function buildForm( campos ){
+		let arrToDOm = FormularioBuilder.build( campos );
+		$("#camposDinamicosDiv").hide().empty().append( arrToDOm ).fadeIn(100);  
+		$("#btnAdd").attr("disabled", false);                                             
 	}
+
+	$("#btnAdd").on("click", () => {
+		validarForm( FormularioBuilder.getElements(), $("#tramitesSelect").val() );
+	});
 
 	function validarForm( campos, id_tramite ){
 		let isValid = true;		
@@ -267,43 +280,14 @@
 			Command: toastr.success("Se agrego el trámite a su lista", "Notifications") ;
 			$("#camposDinamicosDiv").fadeOut(60).empty();
 			$("#tramitesSelect").val("limpia").trigger('change');
+
+			$("#addTramite").modal("hide"); 
+			buildTablaDetalles();
 		}
 	}
 
 	function buildTablaDetalles(){
-		var table = $('#tableDetails').DataTable();
-                table.destroy(); 
-
-		let datosTabla = [];
-		let contador = 0;
-		tramitesGuardar.forEach( (tramiteLista) =>{
-			let tramiteSeleccionado =  tramites.find( tramite => tramite.id_tramite == tramiteLista.id_tramite );
-			tramiteSeleccionado.costo = 0;
-			tramiteSeleccionado.num = contador + 1;
-			datosTabla.push( tramiteSeleccionado );
-		});
-		
-		$("#tableDetails").DataTable( {
-			"paging": false,
-			"searching": false,
-			"info":     false,
-			"language": {
-				"zeroRecords" : "No hay trámites agregados",
-			},
-	        data: datosTabla,
-	        columns: [	           
-	        	{ title: "", data:"id_tramite", render: getTemplateAcciones },
-	        	{ title: "#" , "data":"num"},
-	            { title: "Nombre", data:"tramite" },
-	            { title: "Descripción", data:"tramite" },
-	            { title: "Precio", data:"costo" },
-	        ]
-	    });
-	}
-
-	function getTemplateAcciones( data, type, row, meta  ){
-	    let botonEliminar = "<a type='button' onclick='deleteTramite(" +  JSON.stringify( row ) +")' ><i class='far fa-trash-alt'></i></a>";
-	    return botonEliminar; 
+		ShoppingCarBuilder.build( tramitesGuardar );
 	}
 
 	function deleteTramite( tramite ){
@@ -314,12 +298,90 @@
 	function eliminar(){
 		let tramiteDel = $( "#modalDelete" ).data( "tramite");
 		tramitesGuardar =  tramitesGuardar.filter( tramite => tramite.id_tramite != tramiteDel.id_tramite );
-		var myTable = $('#tableDetails').DataTable();
-		myTable.clear().rows.add(tramitesGuardar).draw();
 		$("#modalDelete").modal('hide');
+		buildTablaDetalles();
 	}
 
-	function next(idNext){
-		$(idNext).click();
+
+	function metodoPago(){
+		$("#metodoPagoBtn").attr("disabled", true);
+		$("#addTramiteBTN").hide();
+
+
+		$("#metodoPagoBtn").append('<div id="spinner-pago" class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>');
+		
+		let url = "https://payments-api-stage.herokuapp.com/v1/pay";
+		$.ajax({
+		  	type: "POST",
+		  	url,
+		  	data: JSONGeneraReferenciaBuilder.build(),
+		   	headers: {
+		        "Authorization":"Bearer B6C8XvbNouJj!ds@.NXjfeswtzehVN",
+		    }
+		}).done((response) => {
+			
+			let cuentas = response.response.cuentas;
+			if(cuentas.length > 0 ){
+				$("#metodoPagoBtn").hide().attr("disabled", false);
+
+
+				$("#divMetodoPago").show();				
+				$("#containerMetodoPago").empty();
+
+				$("#containerMetodoPago").append('<h5 class="mb-4">Método de pago</h5>');
+				cuentas.forEach( (cuenta, index )=> {
+					var image = new Image(45);
+					image.className = "mr-2";
+					image.src = 'data:image/png;base64,' + cuenta.imagen;
+					//document.body.appendChild(image);
+					let divInicial = $("<div>").addClass("form-check");
+					let label = $("<label>").addClass("form-check-label");
+
+					let input = $("<input>").attr({
+						name: "optradio",
+						id: index,
+						type:"radio",
+						value: cuenta.cuenta_id,
+					}).addClass("form-check-input");
+					label.append( input );
+					label.append( image );
+					label.append( getMetodoPago( cuenta.metodopago_id ) );
+					divInicial.append(label);
+					$("#containerMetodoPago").append(divInicial);
+				});
+			}
+			
+		}).fail((rror)=> {
+			console.log( rror)
+		}).always(() => {
+			$("#spinner-pago").remove();
+			//$("#metodoPagoBtn").attr("disabled", false);
+		});
 	}
+
+	function getMetodoPago(metodoId){
+		switch( metodoId ) {
+			case 1:
+				return "Tarjeta de Crédito y Débito";
+			case 2:
+				return "SPEI Interbancario"
+			case 3:
+				return "Ventanilla Bancaria";
+			default:
+				return "";
+		}
+	}
+
+	function cancelarPago(){
+		$("#divMetodoPago").fadeOut(10, () => {
+			$("#metodoPagoBtn").fadeIn();
+			$("#addTramiteBTN").fadeIn();			
+		});	
+
+	}
+
 </script>
+
+							        		
+
+							       
