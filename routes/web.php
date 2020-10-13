17b726621778bad404a990000042b00d390f5a7d
Raymundo Salazar <raymundo.salser@hotmail.com>
2020-10-13 18:04:22 -0500

    create function to redirect on q

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,31 @@ use App\Http\Controllers\ConfirmPasswordController;
 |
 */
 
-Route::middleware(["validate_session"])->group(function(){
-	Route::get('/', function () {
-		return redirect("/dashboard");
-	});
-	Route::get('/dashboard', "DashboardController@index");
-	Route::get('/tramites/{type}', "TramitesController@index");
-	Route::get('/nuevo-tramite', "TramitesController@new");
-	Route::get('/perfil',  "AcountInfoController@index");
-	Route::get('/informacion-cuenta', "ProfileController@index");
-	Route::get('/cambiar-contraseña', "changePassword@index");
-	Route::get('/usuarios', "UsersController@index");
-	
-	// LOGIN
-	Route::get('/login', "LoginController@index");
-	Route::post('/login', "LoginController@validation");
-	Route::get('/logout', "LoginController@logout");
-	Route::get('/password/recovery', "RecoveryController@index");
-	Route::get('/password/recovery/{token}', [ConfirmPasswordController::class,'index'], function($token){
-			return $token;
+Route::get("/", function(){
+	return redirect()->route("dashboard");
+});
+
+Route::group(["prefix" => getenv("APP_PREFIX") ?? "/"], function(){
+	Route::middleware(["validate_session"])->group(function(){
+		Route::get('/', function () {
+			return redirect()->route("dashboard");
+		});
+		Route::get('/dashboard', "DashboardController@index")->name("dashboard");
+		Route::get('/tramites/{type}', "TramitesController@index");
+		Route::get('/nuevo-tramite', "TramitesController@new");
+		Route::get('/perfil',  "AcountInfoController@index");
+		Route::get('/informacion-cuenta', "ProfileController@index");
+		Route::get('/cambiar-contraseña', "changePassword@index");
+		Route::get('/usuarios', "UsersController@index");
+		
+		// LOGIN
+		Route::get('/login', "LoginController@index")->name("login");
+		Route::post('/login', "LoginController@validation");
+		Route::get('/logout', "LoginController@logout");
+		Route::get('/password/recovery', "RecoveryController@index");
+		Route::get('/password/recovery/{token}', [ConfirmPasswordController::class,'index'], function($token){
+				return $token;
+		});
 	});
 });
 
