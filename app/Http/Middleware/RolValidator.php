c2443796856c8d5ec5b458a9d46badb676b5dcda
jaimexcastillo <jaime.castilloc@nuevoleon.gob.mx>
2020-10-22 19:45:41 -0500

    cambios en la tarea, esta funcionando falta cambiar el 403 y el rol_id por el rol_name

--- app/Http/Middleware/RolValidator.php
+++ app/Http/Middleware/RolValidator.php
@@ -16,13 +16,23 @@ class RolValidator
     public function handle($request, Closure $next)
     {
         $validator = [
-            "notary_titular" => [ "dashboard", "perfil" , "tramites", "informacion-cuenta" , "cambiar-contraseña", "usuarios"],
-            "notary_capturist" => ["dashboard", "perfil" , "tramites", "informacion-cuenta" , "cambiar-contraseña"]
-
+            "notary_titular" => [ "/dashboard", "/perfil" , "/tramites", "/informacion-cuenta" , "/cambiar-contraseña", "/usuarios"],
+            "notary_subsitute" => [ "/dashboard", "/perfil" , "/tramites", "/informacion-cuenta" , "/cambiar-contraseña", "/usuarios"],
+            "2" => [ "/dashboard", "/perfil" , "/tramites", "/informacion-cuenta" , "/cambiar-contraseña", "/usuarios"],
+            "notary_capturist" => ["/dashboard", "/perfil" , "/tramites", "/informacion-cuenta" , "/cambiar-contraseña"],
+            "notary_payments" => [ "/perfil" ,  "/informacion-cuenta" , "/cambiar-contraseña"]
         ];
 
+        // dd($request->getPathInfo());
         $session = to_object(session()->get("user"));
-        if( !empty($session) && $session->role_id  )
-        return $next($request);
+        if (!empty($session) && isset($validator[$session->role_id]) ) {
+            if( in_array($request->getPathInfo()  ,$validator[$session->role_id])  ){
+                return $next($request);
+            }else{
+                return response(403);
+            };
+        }else{
+            return response(403);
+        }
     }
 }
