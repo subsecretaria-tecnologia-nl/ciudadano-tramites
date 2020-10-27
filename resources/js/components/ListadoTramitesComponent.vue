e213d764a4d5f5f0a8e1977ef07360185179c901
Alberto <alberto.martinezve@famsa.com>
2020-10-27 16:17:05 -0600

    paginacion de tramites

--- resources/js/components/ListadoTramitesComponent.vue
+++ resources/js/components/ListadoTramitesComponent.vue
@@ -11,7 +11,43 @@
 
 
         <div class="container" v-if="!loading">
-            <tramite-component v-for="(tramite, index) in tramites" :tramite="tramite" v-bind:key="index"></tramite-component>
+            <tramite-component v-for="(tramite, index) in tramitesPaginados" :tramite="tramite" v-bind:key="index"></tramite-component>
+
+            <div class="card card-custom">
+                <div class="card-body py-7">
+                    <!--begin::Pagination-->
+                    <div class="d-flex justify-content-between align-items-center flex-wrap">
+                        <div class="d-flex flex-wrap mr-3">
+                            <a href="#" class="btn btn-icon btn-sm btn-light-primary mr-2 my-1">
+                                <i class="ki ki-bold-double-arrow-back icon-xs"></i>
+                            </a>
+                            <a href="#" class="btn btn-icon btn-sm btn-light-primary mr-2 my-1">
+                                <i class="ki ki-bold-arrow-back icon-xs"></i>
+                            </a>
+                            <a class="btn btn-icon btn-sm border-0 btn-hover-primary mr-2 my-1" v-for="(r) in pages"
+                            v-bind:class="[ currPa === r ? 'active' : '']" v-on:click="goto(r)"> 
+                               {{ r }}
+                            </a>
+                            <a href="#" class="btn btn-icon btn-sm btn-light-primary mr-2 my-1">
+                                <i class="ki ki-bold-arrow-next icon-xs"></i>
+                            </a>
+                            <a href="#" class="btn btn-icon btn-sm btn-light-primary mr-2 my-1">
+                                <i class="ki ki-bold-double-arrow-next icon-xs"></i>
+                            </a>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <select class="form-control form-control-sm text-primary font-weight-bold mr-4 border-0 bg-light-primary" style="width: 75px;" v-model="porPage" @change="calcularPage">
+                                <option value="10">1</option>
+                                <option value="20">2</option>
+                                <option value="30">3</option>
+                            </select>
+                            <span class="text-muted">Mostrando {{ porPage }} de {{ tramites.length }} trámites</span>
+                        </div>
+                    </div>
+                    <!--end:: Pagination-->
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -21,7 +57,7 @@
 
         data() {
             return {
-                tramites: [], loading:true
+                tramites: [], loading:true, porPage : 10, pages:[0], currPa :1
             }
         },
         created() {
@@ -39,10 +75,34 @@
                 try {
                     let response = await axios.get(url);
                     this.tramites = response.data;
+                    
+                    let pagesTotal = Math.ceil( this.tramites.length / this.porPage);
+                    let pages = [];
+
+                    for (var i = 0; i < pagesTotal; i++) {
+                        pages.push( i + 1 );
+                    }
+                    this.pages = pages;
                 } catch (error) {
                     console.log(error);
                 }
+                this.tramitesPaginados = this.tramites.slice( (this.currPa - 1 ) * this.porPage,  ((this.currPa - 1 ) * this.porPage)  + this.porPage );
                 this.loading = false;
+            },
+
+            calcularPage(){
+                let pages = [];
+                let pagesTotal = Math.ceil( this.tramites.length / this.porPage);
+                for (var i = 0; i < pagesTotal; i++) {
+                    pages.push( i + 1 );
+                }
+                this.pages = pages;
+                this.goto(1);
+            },
+
+            goto( page ){ 
+                this.tramitesPaginados = this.tramites.slice( (page - 1 ) * parseInt(this.porPage),  ((page - 1 ) * parseInt(this.porPage)  )  + parseInt(this.porPage) );
+                this.currPa = page;
             }
 
         }
