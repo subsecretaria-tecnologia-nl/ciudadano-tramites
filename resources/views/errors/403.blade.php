a0f312470dfe60de235bb0ea46965e4159e476bd
Raymundo Salazar <raymundo.salser@hotmail.com>
2020-10-30 09:57:47 -0600

    modify

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<base href="{{ getenv("APP_URL") }}">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Página no Encontrada | {{ Config::get("layout.args.title") }}</title>
