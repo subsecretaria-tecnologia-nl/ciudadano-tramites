d67fa38a3ebcab57c53684b7ab8695a25ace5a09
Raymundo Salazar <raymundo.salser@hotmail.com>
2020-10-30 12:25:48 -0600

    fix role validator

--- app/Http/Middleware/RolValidator.php
+++ app/Http/Middleware/RolValidator.php
@@ -15,32 +15,33 @@ class RolValidator
      */
     public function handle($request, Closure $next)
     {
+        $session = to_object(session()->get("user"));
         $validator = [
-            "notary_titular" => ["/getTramites", "/allTramites", "/getCampos", "/crearSolicitud", "/getcostoTramite", "/detalle-tramite", "/carshop", "/", "/dashboard", "/perfil" , "/tramites", "/tramites/pendientes",  "/informacion-cuenta" , "/cambiar-contraseña", "/usuarios", "/logout", "/nuevo-tramite" ],
-            "notary_substitute" => ["/getTramites", "/allTramites", "/getCampos", "/crearSolicitud", "/getcostoTramite", "/detalle-tramite", "/carshop", "/", "/dashboard", "/perfil" , "/tramites", "/tramites/pendientes", "/informacion-cuenta" , "/cambiar-contraseña", "/usuarios", "/logout", "/nuevo-tramite" ],
-            "notary_capturist" => ["/getTramites", "/allTramites", "/getCampos", "/crearSolicitud", "/getcostoTramite", "/detalle-tramite", "/carshop", "/", "/dashboard", "/perfil" , "/tramites", "/informacion-cuenta" , "/cambiar-contraseña", "/logout", "/nuevo-tramite" ],
+            "notary_titular" => ["*"],
+            "notary_substitute" => ["*"],
+            "notary_capturist" => ["/getTramites", "/allTramites", "/getCampos", "/crearSolicitud", "/getcostoTramite", "/detalle-tramite", "/carshop", "/", "/dashboard", "/perfil" , "/tramites", "/informacion-cuenta" , "/cambiar-contraseña", "/logout", "/nuevo-tramite", "/detalle-tramite/(.+)" ],
             "notary_payments" => ["/getTramites", "/allTramites", "/getCampos", "/crearSolicitud", "/getcostoTramite", "/detalle-tramite", "/carshop", "/", "/dashboard", "/perfil" ,  "/informacion-cuenta" , "/cambiar-contraseña", "/tramites/por-pagar", "/logout"]
         ];
 
-        foreach($validator as $key => $val){
-            foreach($val as $k => $route){
-                $validator[$key][$k] = (getenv("APP_PREFIX")??"").$route;
-            }
-        }
+        $session_whitelist = $validator;
+        $path = explode("/", $request->getPathInfo());
+        $path = array_filter($path, function($var){
+            return !empty($var);
+        });
+        $path = implode("/", $path);
 
-        $session = to_object(session()->get("user"));
-        if(!empty($session->role_name)){
-            if (!empty($session) && isset($validator[$session->role_name]) ) {
-                if( in_array($request->getPathInfo(), $validator[$session->role_name])  ){
-                    return $next($request);
-                }else{
-                    return abort(403);    
-                };
-            }else{
-                 return abort(403);
-            }
-        }else{
+        $pass = array_map(function($a) use ($path){
+            if($a == "*") return $a;
+            $whitePath = ((getenv("APP_PREFIX") ? explode("/", getenv("APP_PREFIX"))[1]."" : "").$a);
+            preg_match("/^".str_replace("/", "\/", $whitePath)."$/", $path, $matches);
+            if(!empty($matches))
+                return $a;
+        }, $session_whitelist[$session->role_name]);
+        $pass = array_filter($pass);
+
+        if(empty($pass))
+            return abort(403);
+        else
             return $next($request);
-        }
     }
 }
