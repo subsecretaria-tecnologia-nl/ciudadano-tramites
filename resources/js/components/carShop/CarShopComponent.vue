315c14dd58abb1cc02af055566e344eb62bfe397
Alberto <alberto.martinezve@famsa.com>
2020-10-29 22:23:16 -0600

    obtener lista de tramites

--- resources/js/components/carShop/CarShopComponent.vue
+++ resources/js/components/carShop/CarShopComponent.vue
@@ -4,16 +4,21 @@
 
 		    <!--Grid column-->
 		    <div class="col-lg-8">
-
 		      	<!-- Card -->
 		      	<div v-if="!mostrarMetodos">
-		        	
-		        		<div class="container-fluid mb-4 mb-3 shadow-sm p-3 bg-white rounded" id="listTramites" v-for="tramite in tramitesPaginados" >
+		      		     <v-container v-if="obteniendoTramites">
+			                <v-row>
+			                    <v-col cols="12" md="12">
+			                        <v-skeleton-loader  type="list-item" v-for="(r,i) in [1,2,3,4,5,6]" height="90px" style="margin-bottom: 8px;"></v-skeleton-loader>
+			                    </v-col>
+			                </v-row>
+			            </v-container>
+		        		<div class="container-fluid mb-4 mb-3 shadow-sm p-3 bg-white rounded" id="listTramites" v-for="tramite in tramitesPaginados"  v-if="!obteniendoTramites" >
 		        			<div class="col-md-7 col-lg-9 col-xl-9">
 		        				<div>
 		        					<div class="d-flex justify-content-between">
 		        						<div>
-		        							<h5> {{ tramite.id_tramite }} </h5>
+		        							<h5> {{ tramite.nombre }} </h5>
 		        							<p class="mb-3 text-muted text-uppercase small"> 
 		        								Nombre : {{ tramite.datos_solicitante.nombre }} {{ tramite.datos_solicitante.apellido_paterno  }} {{ tramite.datos_solicitante.apellido_materno }}
 		        							</p>
@@ -29,9 +34,14 @@
 		        							</a>
 		        						</div>
 		        						<p class="mb-0">
-		        							<span><strong id="summary"> 
+		        							<span>
+		        								<strong > 
+		        									$<span v-html="tramite.importe_tramite"> </span>
+		        									<!--
 		        								$ {{ tramite.importe_tramite}}
-		        							</strong></span>
+		        							-->
+		        								</strong>
+		        							</span>
 		        						</p>
 		        					</div>
 		        				</div>
@@ -57,8 +67,8 @@
 	                            </div>
 	                            <div class="d-flex align-items-center">
 	                                <select class="form-control form-control-sm text-primary font-weight-bold mr-4 border-0 bg-light-primary" style="width: 75px;" v-model="porPage" @change="calcularPage">
-	                                    <option value="10">10</option>
-	                                    <option value="20">20</option>
+	                                    <option value="2">2</option>
+	                                    <option value="5">5</option>
 	                                    <option value="30">30</option>
 	                                </select>
 	                            </div>
@@ -66,9 +76,6 @@
 	                        <!--end:: Pagination-->
 	                    </div>
 	                </div>
-
-
-		        	
 
 
 		      	</div>
@@ -88,10 +95,11 @@
 		          		<h5 class="mb-3">Total:</h5>
 
 		          		<ul class="list-group list-group-flush">
+		          			<!--
 		            		<li class="list-group-item d-flex justify-content-between align-items-center px-0 pb-0">
 		              			Subtotal
 		              			<span id="subTotalTramites">$0.00</span>
-		            		</li>
+		            		</li>-->
 		            		<li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
 		              			<div>
 		                			<strong>Total</strong>
@@ -100,8 +108,8 @@
 		                			</strong>
 		              			</div>
 		              			<span>
-		              				<strong id="totalTramites">
-		              					$0.00
+		              				<strong id="totalTramites" >
+		              					${{ totalImporte }}
 		              				</strong>
 		              			</span>
 		            		</li>
@@ -146,6 +154,9 @@
 <script>
 	import { uuid } from 'vue-uuid';
     export default {
+    	props: ['idUsuario'],
+
+
         data() {
             return {
             	diabledBtnMedtodo:false,
@@ -153,215 +164,14 @@
             	mostrarMetodos:false,
             	infoMetodosPago:{},
 
-            	porPage : 10, pages:[0], currentPage :1, tramitesPaginados:{}
+            	porPage : 10, pages:[0], currentPage :1, tramitesPaginados:{},
+            	totalImporte:0,
+            	obteniendoTramites:false
             }
         },
   
         mounted() {
-
-
-           	this.tramites =  [
-			    {
-			      "id_seguimiento": 4254,
-			      "id_tipo_servicio": 397,
-			      "id_tramite": "BMU8605134I81FM5K7D80EGA56944",
-			      "importe_tramite": "3041",
-			      "auxiliar_1": "GRUPOS ICV BMU8605134I8 ",
-			      "auxiliar_2": "",
-			      "auxiliar_3": "",
-			      "datos_solicitante": {
-			        "nombre": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "apellido_paterno": "",
-			        "apellido_materno": "",
-			        "razon_social": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "rfc": "BMU8605134I8 ",
-			        "curp": "",
-			        "email": "",
-			        "calle": "AV LA JUVENTUD",
-			        "colonia": "BOSQUES DEL NOGALAR",
-			        "numexterior": "",
-			        "numinterior": "120",
-			        "municipio": "SAN NICOLAS DE LOS GARZA",
-			        "codigopostal": 66480
-			      },
-			      "datos_factura": {
-			        "nombre": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "apellido_paterno": "",
-			        "apellido_materno": "",
-			        "razon_social": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "rfc": "BMU8605134I8 ",
-			        "curp": "",
-			        "email": "",
-			        "calle": "AV LA JUVENTUD",
-			        "colonia": "BOSQUES DEL NOGALAR",
-			        "numexterior": "",
-			        "numinterior": "120",
-			        "municipio": "SAN NICOLAS DE LOS GARZA",
-			        "codigopostal": 66480
-			      },
-			      "detalle": [
-			        {
-			          "concepto": "REFRENDO PTE.AÑO",
-			          "importe_concepto": "3041",
-			          "partida": 95101
-			          
-			        },
-			        {
-			          "concepto": "SANCION REFRENDO PTE.AÑO",
-			          "importe_concepto": "130",
-			          "partida": 95123,
-			          "descuentos": [
-			            {
-			              "concepto_descuento": "SUBSIDIO PAPV REFRENDO PA",
-			              "importe_descuento": "130",
-			              "partida_descuento": 95136
-			            }
-			          ]
-			        }
-			      ]
-			    },
-			    {
-			      "id_seguimiento":334,
-			      "id_tipo_servicio": 397,
-			      "id_tramite": "BMU8605134I81GBKC34J9WJ108483",
-			      "importe_tramite": "820",
-			      "auxiliar_1": "GRUPOS ICV BMU8605134I8 ",
-			      "auxiliar_2": "",
-			      "auxiliar_3": "",
-			      "datos_solicitante": {
-			        "nombre": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "apellido_paterno": "",
-			        "apellido_materno": "",
-			        "razon_social": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "rfc": "BMU8605134I8 ",
-			        "curp": "",
-			        "email": "",
-			        "calle": "AV LA JUVENTUD",
-			        "colonia": "BOSQUES DEL NOGALAR",
-			        "numexterior": "",
-			        "numinterior": "120",
-			        "municipio": "SAN NICOLAS DE LOS GARZA",
-			        "codigopostal": 66480
-			      },
-			      "datos_factura": {
-			        "nombre": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "apellido_paterno": "",
-			        "apellido_materno": "",
-			        "razon_social": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "rfc": "BMU8605134I8 ",
-			        "curp": "",
-			        "email": "",
-			        "calle": "AV LA JUVENTUD",
-			        "colonia": "BOSQUES DEL NOGALAR",
-			        "numexterior": "",
-			        "numinterior": "120",
-			        "municipio": "SAN NICOLAS DE LOS GARZA",
-			        "codigopostal": 66480
-			      },
-			      "detalle": [
-			        {
-			          "concepto": "REFRENDO PTE.AÑO",
-			          "importe_concepto": "3035",
-			          "partida": 95101,
-			          "descuentos": [
-			            {
-			              "concepto_descuento": "SUBSIDIO ANTIGUEDAD 15 AÑOS",
-			              "importe_descuento": "2215",
-			              "partida_descuento": 95106
-			            }
-			          ]
-			        },
-			        {
-			          "concepto": "SANCION REFRENDO PTE.AÑO",
-			          "importe_concepto": "130",
-			          "partida": 95123,
-			          "descuentos": [
-			            {
-			              "concepto_descuento": "SUBSIDIO PAPV REFRENDO PA",
-			              "importe_descuento": "130",
-			              "partida_descuento": 95136
-			            }
-			          ]
-			        }
-			      ]
-			    },
-			    {
-			      "id_seguimiento":43,
-			      "id_tipo_servicio": 397,
-			      "id_tramite": "BMU8605134I81GBKC34JXWJ103325",
-			      "importe_tramite": "826",
-			      "auxiliar_1": "GRUPOS ICV BMU8605134I8 ",
-			      "auxiliar_2": "",
-			      "auxiliar_3": "",
-			      "datos_solicitante": {
-			        "nombre": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "apellido_paterno": "",
-			        "apellido_materno": "",
-			        "razon_social": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "rfc": "BMU8605134I8 ",
-			        "curp": "",
-			        "email": "",
-			        "calle": "AV LA JUVENTUD",
-			        "colonia": "BOSQUES DEL NOGALAR",
-			        "numexterior": "",
-			        "numinterior": "120",
-			        "municipio": "SAN NICOLAS DE LOS GARZA",
-			        "codigopostal": 66480
-			      },
-			      "datos_factura": {
-			        "nombre": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "apellido_paterno": "",
-			        "apellido_materno": "",
-			        "razon_social": "BEBIDAS MUNDIALES S DE RL DE CV",
-			        "rfc": "BMU8605134I8 ",
-			        "curp": "",
-			        "email": "",
-			        "calle": "AV LA JUVENTUD",
-			        "colonia": "BOSQUES DEL NOGALAR",
-			        "numexterior": "",
-			        "numinterior": "120",
-			        "municipio": "SAN NICOLAS DE LOS GARZA",
-			        "codigopostal": 66480
-			      },
-			      "detalle": [
-			        {
-			          "concepto": "REFRENDO PTE.AÑO",
-			          "importe_concepto": "3041",
-			          "partida": 95101,
-			          "descuentos": [
-			            {
-			              "concepto_descuento": "SUBSIDIO ANTIGUEDAD 15 AÑOS",
-			              "importe_descuento": "2215",
-			              "partida_descuento": 95106
-			            }
-			          ]
-			        },
-			        {
-			          "concepto": "SANCION REFRENDO PTE.AÑO",
-			          "importe_concepto": "130",
-			          "partida": 95123,
-			          "descuentos": [
-			            {
-			              "concepto_descuento": "SUBSIDIO PAPV REFRENDO PA",
-			              "importe_descuento": "130",
-			              "partida_descuento": 95136
-			            }
-			          ]
-			        }
-			      ]
-			    }
-			    
-			];
-
-			this.tramitesFiltrados = this.tramites;
-			let pagesTotal = Math.ceil( this.tramitesFiltrados.length / this.porPage);
-		    let pages = [];
-
-            for (var i = 0; i < pagesTotal; i++) {
-                pages.push( i + 1 );
-            }
-            this.pages = pages;
-            this.pagination(1);
+        	this.obtenerTramitesAgregados();
         },
 
         methods: {
@@ -370,20 +180,29 @@
         		this.tramites = this.tramites.filter( tramite => tramite.id_tramite != id_tramite );
         	},
 
+
 		    metodoPago() {
 		    	this.diabledBtnMedtodo = true;
+		    	const clone = items => items.map(item => (Array.isArray(item) ? clone(item) : item))
+		    	let tramitesAEnviar =clone( this.tramites);
+		    	tramitesAEnviar = tramitesAEnviar.map( tramite =>{
+		    		delete tramite.nombre;
+		    		return tramite;
+		    	})
+
 		    	let url = process.env.PAYMENTS_HOSTNAME  + "/v1/pay"
 				let data = {
 					"token": "DD0FDED2FE302392164520BF7090E1B3BEB7",
 					"referencia": "",
 					"url_retorno": "url",
-					"importe_transaccion": "4687",
+					"importe_transaccion":  this.totalImporte, //"4687",
 					"id_transaccion": uuid.v4(),//"BMU8605134I82915082020",
 					"entidad": 2,
 					"url_confirma_pago": "url",
 					"es_referencia": "1",
-					"tramite": this.tramites 
+					"tramite": tramitesAEnviar
 				}
+				
             	axios.post(url, data, {
             		headers:{
             			"Authorization":"Bearer " + process.env.PAYMENTS_KEY,
@@ -391,13 +210,15 @@
             		},
             	} )
               	.then(response => {
-              		console.log( response.data.response)
+
                 	this.infoMetodosPago = response.data.response;
+                	this.mostrarMetodos = true;
               	}).catch((error)=> {
 					this.diabledBtnMedtodo = false;
+					this.mostrarMetodos = false;
 				}).finally(() => {
 					this.diabledBtnMedtodo = false;
-					this.mostrarMetodos = true;
+					
 				});
 
 	    	},
@@ -434,8 +255,176 @@
 
 
                 this.goto(1);
+
+
             },
 
+
+            async obtenerTramitesAgregados(){
+            	this.obteniendoTramites = true;
+
+                let url = process.env.TESORERIA_HOSTNAME + "/solicitudes-info/" + this.idUsuario;
+                try {
+                    let response = await axios.get(url);
+                    let notary_offices = response.data.notary_offices;
+                    let tramites =  response.data.tramites ;
+                    this.construirJSONTramites( tramites );
+                    this.obteniendoTramites = false;
+                } catch (error) {
+                    console.log(error);
+                    this.obteniendoTramites = false;
+                }
+                //this.loading = false;
+            },
+
+            async obtenerCosto( data ){
+                let url = process.env.APP_URL + "/getcostoTramite";                
+                try {
+                    let response = await axios.post(url, data);
+                    let detalleTramite = response.data;
+                    
+                    return detalleTramite[0];
+                } catch (error) {
+                    console.log(error);
+                    return false;
+                }
+            },
+
+            async construirJSONTramites( tramites ){
+            	let listadoTramites = [];
+            	let requestCostos = [];
+            	let contadorSolicitantes = 0;
+			    tramites.forEach(  tramiteInarray => {
+					
+					tramiteInarray.solicitudes.forEach(  soliciante => {
+						let tramitesJson = {};
+						tramitesJson.nombre = tramiteInarray.tramite;
+						tramitesJson.id_seguimiento = tramiteInarray.tramite_id;
+						tramitesJson.id_tipo_servicio = 397;//tramiteInarray.tramite_id;//397;//
+						tramitesJson.id_tramite = soliciante.clave;
+						tramitesJson.auxiliar_1 = "";
+						tramitesJson.auxiliar_2 = "";
+						tramitesJson.auxiliar_3 = "";
+						tramitesJson.importe_tramite = '<div id="spinner-pago" class="spinner-border spinner-border-sm float-right" role="status" ><span class="sr-only">Loading...</span>	</div>'
+						let info = JSON.parse(soliciante.info);
+						let solicianteInfo = info.solicitante;
+
+						tramitesJson.detalle = [];
+
+						tramitesJson.detalle[0] = { 
+							concepto : info.partida ? info.partidas[0].descripcion : "Descripcion opcional",
+							partida: info.partida ? info.partidas[0].id_partida : 654656546,
+							importe_concepto:1						
+						}
+
+						tramitesJson.datos_solicitante = {
+					        "nombre": solicianteInfo.tipoPersona == "pm" ? "" : solicianteInfo.nombreSolicitante,
+					        "apellido_paterno": solicianteInfo.tipoPersona == "pm" ? "" : solicianteInfo.apPat,
+					        "apellido_materno": solicianteInfo.tipoPersona == "pm" ? "" : solicianteInfo.apMat,
+					        "razon_social": solicianteInfo.tipoPersona == "pm" ? solicianteInfo.razonSocial : "",
+					        "rfc": solicianteInfo.rfc,
+					        "curp": solicianteInfo.curp || "",
+					        "email": solicianteInfo.email|| "",
+					        "calle": solicianteInfo.calle|| "",
+					        "colonia":solicianteInfo.colonia|| "",
+					        "numexterior": solicianteInfo.numexterior|| "",
+					        "numinterior": solicianteInfo.numinterior|| "",
+					        "municipio": solicianteInfo.municipio|| "",
+					        "codigopostal": solicianteInfo.codigopostal|| "",
+					    }
+
+					    tramitesJson.datos_factura = tramitesJson.datos_solicitante;
+
+						let data = {  
+		                    valor_catastral: info.valor_catastral || 0,
+		                    id_seguimiento: soliciante.clave,
+		                    tramite_id: tramiteInarray.tramite_id,
+		                    valor_operacion: info.valor_de_operacion || 0,
+		                    oficio:62
+                		}
+                		
+                		let url = process.env.APP_URL + "/getcostoTramite";
+
+                		requestCostos.push(axios.post(url, data,{headers:{
+                			contadorSolicitantes:contadorSolicitantes
+                		}}));
+                		
+
+						listadoTramites.push( tramitesJson );
+						contadorSolicitantes=contadorSolicitantes+1;
+
+
+					});
+
+			    });
+
+	    		this.tramites = listadoTramites;
+
+				this.tramitesFiltrados = this.tramites;
+				let pagesTotal = Math.ceil( this.tramitesFiltrados.length / this.porPage);
+			    let pages = [];
+
+	            for (var i = 0; i < pagesTotal; i++) {
+	                pages.push( i + 1 );
+	            }
+	            this.pages = pages;
+	            this.pagination(1);
+
+			    axios.all(requestCostos).then(axios.spread((...responses) => {
+
+					responses.forEach( respuesta => {
+
+						let indiceTramite = respuesta.config.headers.contadorSolicitantes;
+						listadoTramites[indiceTramite].importe_tramite = respuesta.data ? respuesta.data[0].costo_final : 1;
+
+
+						listadoTramites[indiceTramite].detalle[0].importe_concepto = listadoTramites[indiceTramite].importe_tramite;
+/*
+						let descuento =  respuesta.data ? respuesta.data[0].descuentos[0] : false;
+						if( descuento ){
+							if(listadoTramites[indiceTramite].detalle){
+								listadoTramites[indiceTramite].detalle[0].importe_concepto = descuento.importe_concepto;
+							}  else {
+								listadoTramites[indiceTramite].detalle[0].importe_concepto = listadoTramites[indiceTramite].importe_tramite;;
+							}
+							
+						}else {
+							listadoTramites[indiceTramite].detalle[0].importe_concepto = listadoTramites[indiceTramite].importe_tramite;
+						}*/
+						
+					});
+					  // use/access the results 
+				})).catch(errors => {
+				  console.log( errors )
+				}).finally( () =>{
+					console.log("termino de consulttar")
+/*
+					listadoTramites = listadoTramites.filter( elTramite => { 
+
+						return elTramite.importe_tramite > 0 && elTramite.detalle && elTramite.detalle[0].concepto != "No aplica"
+					});*/
+//console.log( JSON.parse( listadoTramites ) )
+
+					//this.tramites = listadoTramites;
+					//this.tramitesFiltrados = this.tramites;
+
+					let total = 0;
+					this.tramites.forEach(tramite => total = total + tramite.importe_tramite );
+ 					this.totalImporte = total;
+/*
+					let pagesTotal = Math.ceil( this.tramitesFiltrados.length / this.porPage);
+				    let pages = [];
+
+		            for (var i = 0; i < pagesTotal; i++) {
+		                pages.push( i + 1 );
+		            }
+		            this.pages = pages;
+		            this.pagination(1);*/
+				} )
+
+			    
+
+            }
 
 		}
 
