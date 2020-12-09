bae4f0a3d87bc81fa96f881d5327c1bce490522f
Adriana1705 <adriana.contreras@futurite.com>
2020-12-09 16:12:17 -0600

    Tramites con categoría

---
+++ app/Repositories/PortaltramitecategoriaRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface PortaltramitecategoriaRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface PortaltramitecategoriaRepository extends RepositoryInterface
+{
+    //
+}
