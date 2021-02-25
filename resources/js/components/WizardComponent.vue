9b86a43ffa446b45671ed448ea99e9c6f7dfce13
alberto <albon_marvel@hotmail.com>
2021-02-25 16:48:30 -0600

    nombre tramite en mayuscula

--- resources/js/components/WizardComponent.vue
+++ resources/js/components/WizardComponent.vue
@@ -194,7 +194,7 @@
                   wizardDesc:'Revisar y completar',
                 }],
                 errors:[],
-                nombtreTamite5IS5:'ISR 5% por enajenación de inmuebles'
+                nombtreTamite5IS5:'ISR 5% POR ENAJENACIÓN DE INMUEBLES'
                 //declararEn0:false
             }
         },
